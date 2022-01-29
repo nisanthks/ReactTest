e60cd35a22aa6846b52866fa67144a829a1f7910
nisanthks <nisanthks@gmail.com>
2022-01-29 11:58:55 +0530

    Initial
    first commit

---
+++ precache-manifest.3614338a63a924842bce15cdbb06db77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b22fca189bfd6c74500c",
+    "url": "/static/css/main.5ab231ba.chunk.css"
+  },
+  {
+    "revision": "b22fca189bfd6c74500c",
+    "url": "/static/js/main.c406c1fe.chunk.js"
+  },
+  {
+    "revision": "125fd87a74641d6e3a0a",
+    "url": "/static/js/runtime~main.089da3ca.js"
+  },
+  {
+    "revision": "85aa92a6c5c63f91d0fc",
+    "url": "/static/js/2.08a01a85.chunk.js"
+  },
+  {
+    "revision": "d2d0378d2bb3c05b494e",
+    "url": "/static/js/3.94070b3b.chunk.js"
+  },
+  {
+    "revision": "52ac155ee65f76a68d6a1baa8790e38c",
+    "url": "/static/media/hi.52ac155e.png"
+  },
+  {
+    "revision": "0c3b24b72198954916fcfe405af3b66f",
+    "url": "/index.html"
+  }
+];
